58bf63675ef813506e368a41abf70635dfaabd92
SaltwaterC <saltwaterc@gmail.com>
2016-03-14 16:04:29 +0000

    Bump the version of bundled cookbook.

--- cookbooks/kitchen-docker-host/metadata.rb
+++ cookbooks/kitchen-docker-host/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'saltwaterc@gmail.com'
 license 'MIT'
 description 'Installs/Configures kitchen-docker-host'
 long_description 'Installs/Configures kitchen-docker-host'
-version '0.1.1'
+version '0.1.2'
 
 depends 'yum-epel'
 depends 'yum'
