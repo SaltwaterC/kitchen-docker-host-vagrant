569aec9c3a8b5a878968e497c0f8c804e1c206ff
SaltwaterC <saltwaterc@gmail.com>
2016-03-14 16:04:03 +0000

    Get rid of bridge-nf-call warnings.

--- cookbooks/kitchen-docker-host/recipes/default.rb
+++ cookbooks/kitchen-docker-host/recipes/default.rb
@@ -27,18 +27,18 @@ end
 
 cookbook_file '/lib/systemd/system/docker.service' do
   source 'lib.systemd.system.docker.service'
-  user 'root'
+  owner 'root'
   group 'root'
-  mode '0644'
+  mode 0644
   notifies :run, 'execute[systemctl daemon-reload]', :immediately
   notifies :restart, 'service[docker]', :delayed
 end
 
 cookbook_file '/etc/squid/squid.conf' do
   source 'etc.squid.squid.conf'
-  user 'root'
+  owner 'root'
   group 'root'
-  mode '0644'
+  mode 0644
   notifies :restart, 'service[squid]', :delayed
 end
 
@@ -53,6 +53,19 @@ end
 
 selinux_state 'SELinux Disabled' do
   action :disabled
+end
+
+cookbook_file '/etc/sysctl.d/10-bridge-nf-call.conf' do
+  source 'etc.sysctl.d.10-bridge-nf-call.conf'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :run, 'execute[sysctl-p]', :immediately
+end
+
+execute 'sysctl-p' do
+  command 'sysctl -p'
+  action :nothing
 end
 
 execute 'reboot' do
