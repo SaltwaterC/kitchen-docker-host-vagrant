b95e92d1e5d6bf8c1af255047c8ee98d49a0d8ef
SaltwaterC <saltwaterc@gmail.com>
2017-11-01 13:09:59 +0000

    Issue a docker restart to make sure IP forwarding is being picked up.

--- recipes/default.rb
+++ recipes/default.rb
@@ -37,6 +37,7 @@ end
 ].each do |param|
   sysctl_param param do
     value 1
+    notifies :restart, 'service[docker]', :delayed
   end
 end
 
@@ -49,6 +50,15 @@ cookbook_file '/etc/init.d/zram' do
 end
 
 service 'zram' do
+  action %i[enable start]
+end
+
+service 'firewalld' do
+  action %i[stop disable]
+  notifies :restart, 'service[docker]', :delayed
+end
+
+service 'docker' do
   action %i[enable start]
 end
 
